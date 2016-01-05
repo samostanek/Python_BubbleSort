ce9b9912a3b46468ac194c6d891a78c2a0dbcde9
samostanek <samostanek@gmail.com>
2016-01-05 22:38:48 +0100

    Create

---
+++ BubbleSort.py
@@ -0,0 +1,16 @@
+P = [1, 0, 2, 4, 8, 6]
+
+somethingHapened = True
+
+while somethingHapened :
+    i = -1
+    for R in P :
+        i += 1
+        if i != len(P) :
+            if R > P[i + 1] :
+                H = P[i + 1]
+                P[i + 1] = P[i]
+                P[i] = H
+
+    print P
+        
